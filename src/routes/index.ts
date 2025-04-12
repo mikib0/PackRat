9083666ff58f0e209273b68c7c87a8c2cc86a7f6
Anmol Verma <anmolverma2810@gmail.com>
2025-04-12 23:19:51 +0530

    create dashboard packs api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,6 +6,7 @@ import { chatRoutes } from "./chat";
 import { helloRoutes } from "./hello";
 import { packItemSuggestionsRoutes } from "./pack-item-suggestions";
 import { packsRoutes } from "./packs";
+import { dashboardRoutes } from "./dashboard";
 
 const publicRoutes = new Hono();
 
@@ -24,6 +25,7 @@ protectedRoutes.route("/catalog", catalogRoutes);
 protectedRoutes.route("/packs", packsRoutes);
 protectedRoutes.route("/chat", chatRoutes);
 protectedRoutes.route("/pack-item-suggestions", packItemSuggestionsRoutes);
+protectedRoutes.route("/dashboard", dashboardRoutes);
 
 const routes = new Hono();
 
