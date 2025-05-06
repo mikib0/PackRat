3cc31bf38b8068c6a42f458712d9515a3508b4e4
Anmol Verma <anmolverma2810@gmail.com>
2025-05-06 22:58:45 +0530

    add categories list to pack

--- features/packs/hooks/usePackDetails.ts
+++ features/packs/hooks/usePackDetails.ts
@@ -1,16 +1,21 @@
 import { use$ } from '@legendapp/state/react';
 import { packItemsStore, packsStore } from '~/features/packs/store';
+import { computeCategorySummaries } from '~/utils/computeCategories';
 
 // Hook to get a single pack
 export function usePackDetails(id: string) {
-  const pack = use$(() => {
-    return {
-      ...packsStore[id].get(),
-      items: Object.values(packItemsStore.get()).filter(
-        (item) => item.packId == id && !item.deleted
-      ),
-    };
-  });
+  const pack = use$(() => packsStore[id]?.get());
+  const items = use$(() =>
+    Object.values(packItemsStore.get()).filter((item) => item.packId === id && !item.deleted)
+  );
 
-  return pack;
+  const totalWeight = pack.totalWeight ?? 0;
+
+  const categories = computeCategorySummaries(items, totalWeight);
+
+  return {
+    ...pack,
+    items,
+    categories,
+  };
 }
