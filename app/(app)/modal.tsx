58c05b86089b07bead18ec31468e2fcd3279d1b2
Ibrahim Isa Jajere <ibrahimisajajere274@gmail.com>
2025-05-13 10:33:04 +0100

    fix(modal): replace nativewind stuff with packrat specific content

--- app/(app)/modal.tsx
+++ app/(app)/modal.tsx
@@ -1,31 +1,22 @@
-import { Icon } from '@roninoss/icons';
 import { StatusBar } from 'expo-status-bar';
-import { Linking, Platform, View } from 'react-native';
+import { Image, Platform, View } from 'react-native';
 
 import { Text } from '~/components/nativewindui/Text';
 import { useColorScheme } from '~/lib/useColorScheme';
 
+const LOGO_SOURCE = require('~/assets/packrat-app-icon-gradient.png');
+
 export default function ModalScreen() {
-  const { colors, colorScheme } = useColorScheme();
+  const { colorScheme } = useColorScheme();
   return (
     <>
       <StatusBar
         style={Platform.OS === 'ios' ? 'light' : colorScheme === 'dark' ? 'light' : 'dark'}
       />
       <View className="flex-1 items-center justify-center gap-1 px-12">
-        <Icon name="file-plus-outline" size={42} color={colors.grey} />
+        <Image source={LOGO_SOURCE} className="h-10 w-10 rounded-md" resizeMode="contain" />
         <Text variant="title3" className="pb-1 text-center font-semibold">
-          NativeWindUI
-        </Text>
-        <Text color="tertiary" variant="subhead" className="pb-4 text-center">
-          You can install any of the free components from the{' '}
-          <Text
-            onPress={() => Linking.openURL('https://nativewindui.com')}
-            variant="subhead"
-            className="text-primary">
-            NativeWindUI
-          </Text>
-          {' website.'}
+          PackRat
         </Text>
       </View>
     </>
